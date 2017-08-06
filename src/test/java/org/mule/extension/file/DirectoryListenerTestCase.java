ba583564e0df71d5e03f1cb05b668927e1015129
Rodrigo Merino <rodrigo.merino@mulesoft.com>
2017-08-05 23:16:34 -0300

    MULE-11804: Define APIs to dispatch notifications and register listeners

--- src/test/java/org/mule/extension/file/DirectoryListenerTestCase.java
+++ src/test/java/org/mule/extension/file/DirectoryListenerTestCase.java
@@ -16,10 +16,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mule.extension.file.AllureConstants.FileFeature.FILE_EXTENSION;
 import static org.mule.runtime.core.api.config.MuleProperties.OBJECT_MULE_CONTEXT;
+
 import org.mule.extension.file.internal.DirectoryListener;
 import org.mule.runtime.api.cluster.ClusterService;
 import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.context.notification.ServerNotification;
+import org.mule.runtime.core.api.context.notification.ClusterNodeNotification;
+import org.mule.runtime.core.api.context.notification.NotificationListenerRegistry;
 import org.mule.runtime.core.api.lifecycle.PrimaryNodeLifecycleNotificationListener;
 import org.mule.runtime.extension.api.runtime.source.SourceCallback;
 import org.mule.tck.junit4.AbstractMuleContextTestCase;
@@ -30,6 +32,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
 import io.qameta.allure.Feature;
 
 @Feature(FILE_EXTENSION)
@@ -77,11 +80,12 @@ public class DirectoryListenerTestCase extends AbstractMuleContextTestCase {
 
     directoryListener.onStart(sourceCallback);
 
-    verify(mockMuleContext).registerListener(listenerCaptor.capture());
+    verify(mockMuleContext.getRegistry().lookupObject(NotificationListenerRegistry.class))
+        .registerListener(listenerCaptor.capture());
     PrimaryNodeLifecycleNotificationListener listener = listenerCaptor.getValue();
     assertThat(listener, is(notNullValue()));
 
-    listener.onNotification(mock(ServerNotification.class));
+    listener.onNotification(mock(ClusterNodeNotification.class));
 
     verify(clusterService, times(2)).isPrimaryPollingInstance();
 
