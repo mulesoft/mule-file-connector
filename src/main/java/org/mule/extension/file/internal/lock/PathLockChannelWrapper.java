90162ed60c939476d4eab42971c9529f999693d8
Juan Baca Paunero <Bardammu@users.noreply.github.com>
2018-09-26 12:44:41 -0300

    MULE-15687: File Directory Listener leaks open files (#85)

---
+++ src/main/java/org/mule/extension/file/internal/lock/PathLockChannelWrapper.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.file.internal.lock;
+
+import static java.lang.String.format;
+
+import org.mule.extension.file.common.api.lock.PathLock;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Composite {@link PathLock} implementation and {@link FileChannel} that closes the {@link FileChannel} when the
+ * {@link PathLock} is released.
+ *
+ * @since 2.0.0
+ */
+public class PathLockChannelWrapper implements PathLock {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PathLockChannelWrapper.class);
+
+  private PathLock pathLock;
+  private FileChannel fileChannel;
+
+  public PathLockChannelWrapper(PathLock pathLock, FileChannel fileChannel) {
+    this.pathLock = pathLock;
+    this.fileChannel = fileChannel;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean tryLock() {
+    return pathLock.tryLock();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isLocked() {
+    return pathLock.isLocked();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void release() {
+    pathLock.release();
+    try {
+      fileChannel.close();
+    } catch (IOException e) {
+      if (LOGGER.isDebugEnabled()) {
+        LOGGER.debug(format("Found exception attempting to close the channel for the lock on path '%s'", pathLock.getPath()), e);
+      }
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Path getPath() {
+    return pathLock.getPath();
+  }
+
+}
---
+++ src/test/java/org/mule/extension/file/internal/FileInputStreamTestCase.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.file.internal;
+
+import static java.nio.file.StandardOpenOption.READ;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.mule.extension.file.api.LocalFileAttributes;
+import org.mule.extension.file.common.api.lock.NullPathLock;
+import org.mule.extension.file.common.api.lock.PathLock;
+import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import java.io.File;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class FileInputStreamTestCase extends AbstractMuleTestCase {
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  @Test
+  public void channelIsClosedOnPathLockRelease() throws Exception {
+    File file = temporaryFolder.newFile();
+
+    Path path = file.toPath();
+    LocalFileAttributes fileAttributes = new LocalFileAttributes(file.toPath());
+    PathLock pathLock = new NullPathLock(path);
+    FileChannel channel = FileChannel.open(path, READ);
+
+    FileChannel spyFileChannel = spy(channel);
+
+    FileInputStream fileInputStream = new FileInputStream(spyFileChannel, pathLock, path, 10L, fileAttributes);
+    fileInputStream.close();
+
+    verify(spyFileChannel).close();
+  }
+
+}
