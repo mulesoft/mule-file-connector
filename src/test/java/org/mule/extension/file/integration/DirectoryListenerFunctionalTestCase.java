4bb8375dd06f066add8a5ec31ef49d38c011dc3c
Juan Baca Paunero <Bardammu@users.noreply.github.com>
2018-09-26 12:51:25 -0300

    MULE-15687: File Directory Listener leaks open files (#85)

--- src/test/java/org/mule/extension/file/integration/DirectoryListenerFunctionalTestCase.java
+++ src/test/java/org/mule/extension/file/integration/DirectoryListenerFunctionalTestCase.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.extension.file;
+package org.mule.extension.file.integration;
 
 import static java.nio.file.Files.setLastModifiedTime;
 import static java.time.LocalDateTime.now;
--- src/test/java/org/mule/extension/file/integration/FileCopyTestCase.java
+++ src/test/java/org/mule/extension/file/integration/FileCopyTestCase.java
@@ -4,7 +4,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.extension.file;
+package org.mule.extension.file.integration;
 
 import io.qameta.allure.Feature;
 import org.junit.Test;
