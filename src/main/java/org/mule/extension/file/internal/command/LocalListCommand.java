9557cf205d0948049c043816c3383679efec2d89
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2019-04-26 08:41:28 -0300

    MULE-16766: NoSuchFileExceptions with file listeners in clustered environment

--- src/main/java/org/mule/extension/file/internal/command/LocalListCommand.java
+++ src/main/java/org/mule/extension/file/internal/command/LocalListCommand.java
@@ -7,20 +7,26 @@
 package org.mule.extension.file.internal.command;
 
 import static java.lang.String.format;
+
 import org.mule.extension.file.api.LocalFileAttributes;
 import org.mule.extension.file.common.api.FileConnectorConfig;
 import org.mule.extension.file.common.api.command.ListCommand;
 import org.mule.extension.file.common.api.exceptions.FileAccessDeniedException;
 import org.mule.extension.file.internal.LocalFileSystem;
+import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A {@link LocalFileCommand} which implements the {@link ListCommand}
@@ -28,6 +34,8 @@ import java.util.function.Predicate;
  * @since 1.0
  */
 public final class LocalListCommand extends LocalFileCommand implements ListCommand<LocalFileAttributes> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LocalListCommand.class);
 
   private final LocalReadCommand readCommand;
 
@@ -86,21 +94,32 @@ public final class LocalListCommand extends LocalFileCommand implements ListComm
 
     for (File child : parent.listFiles()) {
       Path path = child.toPath();
-      LocalFileAttributes attributes = new LocalFileAttributes(path);
+      try {
+        LocalFileAttributes attributes = new LocalFileAttributes(path);
+        if (child.isDirectory()) {
+          if (matcher.test(attributes)) {
+            accumulator.add(Result.<InputStream, LocalFileAttributes>builder().output(null).attributes(attributes).build());
+          }
 
-      if (child.isDirectory()) {
-        if (matcher.test(attributes)) {
-          accumulator.add(Result.<InputStream, LocalFileAttributes>builder().output(null).attributes(attributes).build());
+          if (recursive) {
+            doList(config, child, accumulator, recursive, matcher, timeBetweenSizeCheck);
+          }
+        } else {
+          if (matcher.test(attributes)) {
+            accumulator.add(readCommand.read(config, attributes, false, timeBetweenSizeCheck));
+          }
         }
 
-        if (recursive) {
-          doList(config, child, accumulator, recursive, matcher, timeBetweenSizeCheck);
-        }
-      } else {
-        if (matcher.test(attributes)) {
-          accumulator.add(readCommand.read(config, attributes, false, timeBetweenSizeCheck));
+      } catch (MuleRuntimeException e) {
+        if (e.getCause() instanceof NoSuchFileException) {
+          LOGGER
+              .debug("A file with path %s was found while listing but was not found when trying to open a file channel to access the file",
+                     path.toString());
+        } else {
+          throw e;
         }
       }
     }
   }
+
 }
