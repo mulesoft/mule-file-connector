2cbdd374febd78f85f07369854cf515d2dd1bbb4
Roger Wang <rogerwang@mulesoft.com>
2022-08-17 14:21:05 -0700

    revert changes for W-11427407

--- src/main/java/org/mule/extension/file/internal/source/PostActionGroup.java
+++ src/main/java/org/mule/extension/file/internal/source/PostActionGroup.java
@@ -52,26 +52,14 @@ public class PostActionGroup extends AbstractPostActionGroup {
   @Optional(defaultValue = "true")
   private boolean applyPostActionWhenFailed = true;
 
-  /**
-   * Whether the target file should be overwritten when the destination has the file with same name
-   */
-  @Parameter
-  @Optional(defaultValue = "false")
-  private boolean overwrite = false;
 
   public PostActionGroup() {}
 
   public PostActionGroup(boolean autoDelete, String moveToDirectory, String renameTo, boolean applyPostActionWhenFailed) {
-    this(autoDelete, moveToDirectory, renameTo, applyPostActionWhenFailed, false);
-  }
-
-  public PostActionGroup(boolean autoDelete, String moveToDirectory, String renameTo, boolean applyPostActionWhenFailed,
-                         boolean overwrite) {
     this.autoDelete = autoDelete;
     this.moveToDirectory = moveToDirectory;
     this.renameTo = renameTo;
     this.applyPostActionWhenFailed = applyPostActionWhenFailed;
-    this.overwrite = overwrite;
   }
 
   public boolean isAutoDelete() {
@@ -88,10 +76,6 @@ public class PostActionGroup extends AbstractPostActionGroup {
 
   public boolean isApplyPostActionWhenFailed() {
     return applyPostActionWhenFailed;
-  }
-
-  public boolean getOverwrite() {
-    return overwrite;
   }
 
 }
