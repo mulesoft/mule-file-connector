b3700242717a405f6a33e7ccdf7f581f70a1a44e
sankarv_sfemu <194505413+sankarv_sfemu@users.noreply.github.com>
2025-06-25 14:04:15 +0530

    Test commit

--- src/test/java/org/mule/extension/file/internal/FileInputStreamTestCase.java
+++ src/test/java/org/mule/extension/file/internal/FileInputStreamTestCase.java
@@ -42,6 +42,7 @@ public class FileInputStreamTestCase extends AbstractMuleTestCase {
     FileInputStream fileInputStream = new FileInputStream(spyFileChannel, pathLock, path, 10L, fileAttributes);
     fileInputStream.close();
 
+    
     verify(spyFileChannel).close();
   }
 
